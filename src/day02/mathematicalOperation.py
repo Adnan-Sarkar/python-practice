d19ef0b976ef506d5da200adb0944e8a24481ab0
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-22 20:03:22 +0600

    Mathematical operations in python

---
+++ src/day02/mathematicalOperation.py
@@ -0,0 +1,17 @@
+# Addition
+print(5 + 6)
+
+# Subtraction
+print(10 - 2)
+
+# Multiplication
+print(3 * 6)
+
+# Division
+print(6 / 3)
+# division gives floating point number
+
+# Exponential
+print(2 ** 3)
+
+# PEMDAS - (), **, * /, + -
