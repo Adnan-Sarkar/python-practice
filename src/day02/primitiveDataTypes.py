6173de73d1bf2e77ef1f29f34ee316abff5a8be8
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-22 19:20:12 +0600

    Primitive data types in python

---
+++ src/day02/primitiveDataTypes.py
@@ -0,0 +1,27 @@
+# Primitive Data Types
+
+# String
+name = "Mango"
+print(name)
+print(name[0])
+print(name[-1])
+
+# Integer
+age = 26
+bigNumber = 123_456_789
+print(age)
+print(age + 10)
+print(bigNumber)
+
+# Float
+cgpa = 3.5
+pi = 3.14159
+print(cgpa)
+print(cgpa + 2)
+print(pi)
+
+# Boolean
+isOkk = True
+isRaining = False
+print(isOkk)
+print(isRaining)
