19b6c00ff884d2688fe33739ae9d948be31b422c
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-22 20:31:06 +0600

    Round a number, floor division and F String in python

---
+++ src/day02/numberManipulation.py
@@ -0,0 +1,15 @@
+# round number
+print(round(8 / 3))
+
+# round two decimal place
+print(round(8 / 3, 2))
+print(round(2.6666666, 2))
+
+# floor division
+print(round(8 // 3))
+
+
+# F String
+number = 10
+
+print(f"Your number is: {number}")
