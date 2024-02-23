3d2698e90d2a95b46c4792a03a6fdb159dd587ce
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-23 14:39:24 +0600

    Leap year calculator using if-elif-else

---
+++ src/day03/leap-year-calculator.py
@@ -0,0 +1,12 @@
+year = int(input("Enter year: "))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print(f"{year} is Leap Year")
+    else:
+      print(f"{year} is not Leap Year")
+  else:
+    print(f"{year} is Leap Year")
+else:
+  print(f"{year} is not Leap Year")
