2d6d8a5b0ad75f082cf91949b2783bec78599874
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-02-26 05:13:26 +0600

    Function in python

---
+++ src/day06/function.py
@@ -0,0 +1,12 @@
+# python built in functions
+print("Hello World!")
+len("Hello World!")
+int("123")
+# input("Enter your name")
+
+# user define functions
+
+def myFunction():
+  print("Hello")
+
+myFunction()
