2d337f881ed856f05d95dadce8afb19a5bf7bb6d
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-03-02 17:58:55 +0600

    Nesting lists and dictionaries

---
+++ src/day09/nesting-lists-and-dictionaries.py
@@ -0,0 +1,26 @@
+# nesting dictionary in a dictionary
+travelLog = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"]
+  },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+  "Bangladesh": {
+    "cities_visited": ["Dhaka", "Rangpur", "Rajsahi"]
+  },
+}
+
+# nesting dictionary in a list
+travelLog = [
+  {
+    "country": "France",
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 3
+  },
+  {
+    "country": "Bangladesh",
+    "cities_visited": ["Dhaka", "Rangpur", "Rajsahi"],
+    "total_visits": 3
+  },
+]
